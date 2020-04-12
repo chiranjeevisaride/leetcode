a29881aa00a2c8f413dd47fc97ac14e6edfda5db
Chiranjeevi Saride <chiranjeevisaride@gmail.com>
2020-04-12 16:10:18 -0400

    Valid Palindrome

---
+++ 125. Valid Palindrome.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if(s == null || s.length()<=1)  return true;
+        int left = 0, right = s.length()-1;
+        while(left < right) {
+            if(Character.isWhitespace(s.charAt(left)) || !Character.isLetterOrDigit(s.charAt(left))){ left++; continue;}
+            if(Character.isWhitespace(s.charAt(right)) || !Character.isLetterOrDigit(s.charAt(right))) { right--; continue;}
+            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) return false;
+            left++; right--;
+        }
+        return true;
+    }
+}
