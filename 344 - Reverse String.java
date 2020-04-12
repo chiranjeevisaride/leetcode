b6d2e4aa06cccfb363ded2d294af52fa0ea1fdfc
Chiranjeevi Saride <chiranjeevisaride@gmail.com>
2020-04-11 21:12:35 -0400

    2nd approach

--- 344 - Reverse String.java
+++ 344 - Reverse String.java
@@ -9,3 +9,14 @@ class Solution {
         }
     }
 }
+
+// 2nd Approach
+class Solution {
+    public void reverseString(char[] s) {
+        for(int i=0; i< s.length/2; i++){
+             char temp = s[i];
+            s[i] = s[s.length-1-i];
+            s[s.length-1-i] = temp;
+        }
+    }
+}
