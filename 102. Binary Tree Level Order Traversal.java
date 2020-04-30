bffc475d841363b024f35cf84ef77ffcf1356503
Chiranjeevi Saride <chiranjeevisaride@gmail.com>
2020-04-30 07:21:47 -0400

    BST zigzig level order traversal

---
+++ 102. Binary Tree Level Order Traversal.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(root == null) return res;
+        Queue<TreeNode> queue = new LinkedList<>();
+        Set<Integer> visited = new HashSet<Integer>();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> levelNodes = new ArrayList<Integer>();
+            for(int i=0; i<size; i++){
+                TreeNode current = queue.poll();
+                levelNodes.add(current.val);
+                if(!visited.contains(current.val)){
+                    visited.add(current.val);
+                } 
+                if(current.left != null) queue.add(current.left);
+                if(current.right != null) queue.add(current.right);
+            }
+            res.add(levelNodes);
+        }
+        
+        return res;
+    }
+}
