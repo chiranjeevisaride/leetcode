31bf2ca8fe13ef36c7fe59225d54bd1f4a162207
Chiranjeevi Saride <chiranjeevisaride@gmail.com>
2020-05-18 19:16:21 -0400

    Numbers With Equal Digit Sum

---
+++ Microsoft Online Assessment/Numbers With Equal Digit Sum.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Main {
+  public static void main(String[] args) {
+    Main m = new Main();
+    int[] A = new int[]{51, 71, 17,42};
+    System.out.println(m.numberWithEqualDigits(A));
+  }
+
+  public int numberWithEqualDigits(int[] A){
+    int result = -1;
+    Map<Integer, Integer> hMap = new HashMap<Integer, Integer>();
+    for(int i=0; i< A.length; i++){
+      int sum = findSumFromDigit(A[i]);
+      if(hMap.containsKey(sum)){
+          hMap.put(sum, hMap.get(sum) + A[i]);
+          if(result == -1 || hMap.get(sum) > result)
+              result = hMap.get(sum);
+      }
+       else
+         hMap.put(sum, A[i]);   
+    }
+    return result;
+  }
+
+  public int findSumFromDigit(int digit){
+    int sum = 0;
+    digit = Math.abs(digit);
+    while(digit !=0){
+      sum += (digit%10);
+      digit = digit/10;
+    }
+    return sum;
+  }
+}
