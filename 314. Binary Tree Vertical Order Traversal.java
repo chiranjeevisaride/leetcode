a4636eb97131457da1dd7297ed88a1bb7bcb96a6
Chiranjeevi Saride <chiranjeevisaride@gmail.com>
2020-07-06 09:45:58 -0400

    Binary Tree Vertical Order Traversal

--- 314. Binary Tree Vertical Order Traversal.java
+++ 314. Binary Tree Vertical Order Traversal.java
@@ -1,45 +1,71 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+// Approach1: BFS 
+// Time - O(nlogn) Space - (n)
 class Solution {
     
-    class MyNode {
-        TreeNode node;
-        int depth;
-        MyNode(TreeNode node, int depth) {
-            this.node = node;
-            this.depth = depth;
-        }
-    }
-    
     public List<List<Integer>> verticalOrder(TreeNode root) {
-        if(root == null) return new ArrayList();
+        List<List<Integer>> result = new ArrayList<>();
+        if(root == null) return result;
         Map<Integer, List<Integer>> map = new TreeMap<>();
-        Queue<MyNode> queue = new LinkedList<>();
-        queue.offer(new MyNode(root, 0));
+        Queue<Pair<TreeNode, Integer>> queue = new LinkedList<>();
+        queue.offer(new Pair(root, 0));
         while(!queue.isEmpty()) {
-            MyNode currentNode = queue.poll();
-            map.putIfAbsent(currentNode.depth, new ArrayList<>());
-            map.get(currentNode.depth).add(currentNode.node.val);
-            if(currentNode.node.left != null) {
-                queue.offer(new MyNode(currentNode.node.left, currentNode.depth -1));
+            Pair<TreeNode, Integer> currentNode = queue.poll();
+            int column = currentNode.getValue();
+            root = currentNode.getKey();
+            
+            map.putIfAbsent(column, new ArrayList<>());
+            map.get(column).add(root.val);
+            
+            if(root.left != null) {
+                queue.offer(new Pair(root.left, column-1));
             }
-            if(currentNode.node.right != null) {
-                queue.offer(new MyNode(currentNode.node.right, currentNode.depth + 1));
+            if(root.right != null) {
+                 queue.offer(new Pair(root.right, column+1));
             }
         }
         return new ArrayList<>(map.values());
+    }
+}
+
+
+// Approach1: BFS + without sorting
+// Time - O(n) Space - O(n)
+
+class Solution {
+
+    public List<List<Integer>> verticalOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(root == null) return result;
+        
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        Queue<Pair<TreeNode, Integer>> queue = new LinkedList<>();
+        int column = 0;
+        queue.offer(new Pair(root, column));
+        int maxColumn = 0, minColumn = 0;
+        
+        while(!queue.isEmpty()) {
+            Pair<TreeNode, Integer> currentNode = queue.poll();
+            column = currentNode.getValue();
+            root = currentNode.getKey();
+            
+            map.putIfAbsent(column, new ArrayList<>());
+            map.get(column).add(root.val);
+            
+            minColumn = Math.min(minColumn, column);
+            maxColumn = Math.max(maxColumn, column);
+            
+            if(root.left != null) {
+                queue.offer(new Pair(root.left, column-1));
+            }
+            if(root.right != null) {
+                queue.offer(new Pair(root.right, column + 1));
+            }
+        }
+        
+        for(int i = minColumn; i <= maxColumn; i++) {
+            result.add(map.get(i));
+        }
+        
+        return result;
     }
 }
