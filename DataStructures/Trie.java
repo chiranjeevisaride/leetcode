f1943505363fd05d078169079b247ae92dc50820
Chiranjeevi Saride <chiranjeevisaride@gmail.com>
2020-09-16 21:17:23 -0400

    trie ds

--- DataStructures/Trie.java
+++ DataStructures/Trie.java
@@ -1,121 +1,178 @@
-/* 
-
-
-Given an array of integers and an integer k, you need to find the total number of continuous subarrays whose sum equals to k.
-
-
-[1,2,2,1,3,4,6]  k = 4    o/p: 3
- 
-[2,2,2,4,2,2]   k = 4
-
-
-*/
-
 import java.io.*;
 import java.util.*;
- 
 
-public class Trie {
-
-    private class TrieNode {
-        Map<Character, TrieNode> children;
-        boolean endOfWord;
-        public TrieNode() {
-            children = new HashMap<>();
-            endOfWord = false;
-        }
+class TrieNode
+{
+    TrieNode[] children;
+    boolean isEndWord; //will be true if the node represents the end of word
+    static final int ALPHABET_SIZE = 26; //Total # of English Alphabets = 26
+    TrieNode(){
+        this.isEndWord = false;
+        this.children = new TrieNode[ALPHABET_SIZE]; 
     }
 
-    private final TrieNode root;
-  
-    public Trie() {
-        root = new TrieNode();
+    //Function to mark the currentNode as Leaf
+    public void markAsLeaf(){
+        this.isEndWord = true;
     }
 
-    /**
-     * Iterative implementation of insert into trie
-     */
-    public void insert(String word) {
-        TrieNode current = root;
-        for (int i = 0; i < word.length(); i++) {
-            char ch = word.charAt(i);
-            TrieNode node = current.children.get(ch);
-            if (node == null) {
-                node = new TrieNode();
-                current.children.put(ch, node);
-            }
-            current = node;
-        }
-        //mark the current nodes endOfWord as true
-        current.endOfWord = true;
+    //Function to unMark the currentNode as Leaf
+    public void unMarkAsLeaf(){
+        this.isEndWord = false;
     }
-
-    /**
-     * Iterative implementation of search into trie.
-     */
-    public boolean search(String word) {
-        TrieNode current = root;
-        for (int i = 0; i < word.length(); i++) {
-            char ch = word.charAt(i);
-            TrieNode node = current.children.get(ch);
-            //if node does not exist for given char then return false
-            if (node == null) {
-                return false;
-            }
-            current = node;
-        }
-        //return true of current's endOfWord is true else return false.
-        return current.endOfWord;
-    }
-
-   
-    /**
-     * Delete word from trie.
-     */
-    public void delete(String word) {
-        delete(root, word, 0);
-    }
-
-    /**
-     * Returns true if parent should delete the mapping
-     */
-    private boolean delete(TrieNode current, String word, int index) {
-        if (index == word.length()) {
-            //when end of word is reached only delete if currrent.endOfWord is true.
-            if (!current.endOfWord) {
-                return false;
-            }
-            current.endOfWord = false;
-            //if current has no other mapping then return true
-            return current.children.size() == 0;
-        }
-        char ch = word.charAt(index);
-        TrieNode node = current.children.get(ch);
-        if (node == null) {
-            return false;
-        }
-        boolean shouldDeleteCurrentNode = delete(node, word, index + 1);
-
-        //if true is returned then delete the mapping of character and trienode reference from map.
-        if (shouldDeleteCurrentNode) {
-            current.children.remove(ch);
-            //return true if no mappings are left in the map.
-            return current.children.size() == 0;
-        }
-        return false;
-    }
-  
-  
-   public static void main(String args[]) {
-      Solution trie = new Solution();
-      trie.insert("abc");
-      trie.insert("abcd");
-      trie.insert("abgl");
-     
-      System.out.println("Search abc: " + trie.search("abc"));
-      System.out.println("Search abcde: " + trie.search("abcde"));
-     
-      System.out.println("Delete abc: " + trie.search("abc"));
-     
-   }  
 }
+
+
+class Trie {
+    
+  TrieNode root;
+  
+  Trie() {
+    root = new TrieNode();
+  }  
+  
+  private int getIndex(char i) {
+    return i - 'a';
+  }  
+  
+    public void insert(String key) {
+        //Null keys are not allowed
+        if (key == null) {
+            return;
+        }
+        key = key.toLowerCase(); //Keys are stored in lowercase
+        TrieNode currentNode = this.root;
+        int index = 0; //to store character index
+
+        //Iterate the Trie with given character index,
+        //If it is null, then simply create a TrieNode and go down a level
+        for (int level = 0; level < key.length(); level++) {
+            index = getIndex(key.charAt(level));
+
+            if (currentNode.children[index] == null) {
+                currentNode.children[index] = new TrieNode();
+            }
+            currentNode = currentNode.children[index];
+        }
+        //Mark the end character as leaf node
+        currentNode.markAsLeaf();
+    }
+  
+  
+  public boolean search(String key) {
+
+        if (key == null || root == null) return false; //Null Key
+
+        key = key.toLowerCase();
+        TrieNode currentNode = this.root;
+        int index = 0;
+
+        //Iterate the Trie with given character index,
+        //If it is null at any point then we stop and return false
+        //We will return true only if we reach leafNode and have traversed the
+        //Trie based on the length of the key
+
+        for (int level = 0; level < key.length(); level++) {
+            index = getIndex(key.charAt(level));
+            if (currentNode.children[index] == null) return false;
+            currentNode = currentNode.children[index];
+        }
+       
+        return currentNode.isEndWord;
+    }
+  
+  //Helper Function to return true if currentNode does not have any children
+    private boolean hasNoChildren(TrieNode currentNode){
+        for (int i = 0; i < currentNode.children.length; i++){
+            if ((currentNode.children[i]) != null)
+                return false;
+        }
+        return true;
+    }
+  
+ 
+   //Recursive function to delete given key
+    private boolean deleteHelper(String key, TrieNode currentNode, int length, int level)
+    {
+        boolean deletedSelf = false;
+
+        if (currentNode == null){
+            System.out.println("Key does not exist");
+            return deletedSelf;
+        }
+
+        //Base Case: If we have reached at the node which points to the alphabet at the end of the key.
+        if (level == length){
+            //If there are no nodes ahead of this node in this path
+            //Then we can delete this node
+            if (hasNoChildren(currentNode)){
+                currentNode = null;
+                deletedSelf = true;
+            }
+            //If there are nodes ahead of currentNode in this path
+            //Then we cannot delete currentNode. We simply unmark this as leaf
+            else
+            {
+                currentNode.unMarkAsLeaf();
+                deletedSelf = false;
+            }
+        }
+        else
+        {
+            TrieNode childNode = currentNode.children[getIndex(key.charAt(level))];
+            boolean childDeleted = deleteHelper(key, childNode, length, level + 1);
+            if (childDeleted)
+            {
+                //Making children pointer also null: since child is deleted
+                currentNode.children[getIndex(key.charAt(level))] = null;
+                //If currentNode is leaf node that means currntNode is part of another key
+                //and hence we can not delete this node and it's parent path nodes
+                if (currentNode.isEndWord){
+                    deletedSelf = false;
+                }
+                //If childNode is deleted but if currentNode has more children then currentNode must   be part of another key.
+                //So, we cannot delete currenNode
+                else if (!hasNoChildren(currentNode)){
+                    deletedSelf = false;
+                }
+                //Else we can delete currentNode
+                else{
+                    currentNode = null;
+                    deletedSelf = true;
+                }
+            }
+            else
+            {
+                deletedSelf = false;
+            }
+        }
+        return deletedSelf;
+    }
+
+    //Function to delete given key from Trie
+    public void delete(String key){
+        if ((root == null) || (key == null)){
+            System.out.println("Null key or Empty trie error");
+            return;
+        }
+        deleteHelper(key, root, key.length(), 0);
+    }
+  
+}  
+
+class Solution {
+  public static void main(String[] args) {
+    Trie trie = new Trie();
+    trie.insert("ant");
+    System.out.println("Inserted ant");
+    trie.insert("and");
+    System.out.println("Inserted and");
+    System.out.println("Search and: " + trie.search("and"));
+    System.out.println("Search ann: " + trie.search("ann"));
+    System.out.println("Delete ant");
+    trie.delete("ant");
+    System.out.println("Search ant: " + trie.search("ant"));
+    
+  }
+}
+
