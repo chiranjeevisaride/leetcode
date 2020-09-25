a17a6206d032084ac4e853c00f8541e9a491f7d4
Chiranjeevi Saride <chiranjeevisaride@gmail.com>
2020-09-25 08:10:34 -0400

    knapsack

--- DynamicProgramming/KnapSackBottomUp.java
+++ DynamicProgramming/KnapSackBottomUp.java
@@ -3,39 +3,27 @@ import java.util.Arrays;
 
 public class KnapSackBottomUp {
 
-    public static void main(String[] args) {
-        int[] wt = new int[]{1,3,4,5};
-        int[] val = new int[]{1,4,5,7};
-        int W = 7;
-        System.out.println(knapsack(wt, val, W , val.length));
-    }
-
-    public static int knapsack(int[] wt, int[] val, int W, int n){
-        int[][] dp = new int[n+1][W+1];
-        int result = knapsackRec(wt,val, W ,n, dp);
-        for(int i=1; i<=n ; i++){ 
-            for(int j=1; j<=W; j++){
-                System.out.print(dp[i][j]);
-            }
-            System.out.println();
-        }      
-        return result;
+    public int kanpsack(int[] weights, int[] profits, int capacity) {
+        if(capacity <=0 || profits.length == 0 || weights.length != profits.length)
+          return 0;
+        int n = weights.length;
+        int[][] dp = new int[n + 1][capacity + 1];
+        for(int i = 1; i <= n; i++) {
+          for(int c = 1; c <= capacity; c++) {
+             if(weights[i-1] <= c)
+                dp[i][c] = Math.max(profits[i-1] + dp[i-1][c-weights[i-1]], dp[i-1][c]);
+             else
+               dp[i][c] = dp[i-1][c];
+          }  
+        }  
+        return dp[n][capacity];
+      }  
+     
+     public static void main(String[] args) {
+       Solution s = new Solution();
+       int[] profits = {1,6,10,16};
+       int[] weights = {1,2,3,5};
+       System.out.println(s.kanpsack(weights, profits, 7));   
      }
- 
- 
-     public static int knapsackRec(int[] wt, int[] val, int W, int n, int dp[][]){
-        for(int i=1; i<=n ; i++){ 
-            for(int j=1; j<=W; j++){
-                if(i == 0 || j== 0) dp[i][j] = 0;
-                else if(wt[i-1] <= j)
-                    dp[i][j] = max( val[i-1] + dp[i-1][j-wt[i-1]], dp[i-1][j]);
-                else
-                     dp[i][j] = dp[i-1][j];     
-            }
-        } 
-        return dp[n][W];
-     }
- 
-     static int max(int a, int b) { return (a > b)? a : b; } 
 
 }
