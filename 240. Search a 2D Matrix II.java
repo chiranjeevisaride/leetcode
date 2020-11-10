bcc624b73091e4ad443be3da33c7de81d1fa4cdb
Chiranjeevi Saride <chiranjeevisaride@gmail.com>
2020-11-10 18:29:33 -0500

    240. Search a 2D Matrix II

--- 240. Search a 2D Matrix II.java
+++ 240. Search a 2D Matrix II.java
@@ -1,3 +1,5 @@
+// Time complexity : O(n+m)
+// Space O(1)
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         if(matrix == null || matrix.length == 0 || matrix[0].length == 0) return false;
