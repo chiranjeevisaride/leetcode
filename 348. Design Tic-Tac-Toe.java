404831b1076da0292254c7239673767f4369d51d
Chiranjeevi Saride <chiranjeevisaride@gmail.com>
2020-11-28 10:32:00 -0500

    Design Tic-Tac-Toe

--- 348. Design Tic-Tac-Toe.java
+++ 348. Design Tic-Tac-Toe.java
@@ -1,14 +1,17 @@
-class TicTacToe {
+import java.io.*;
+import java.util.*;
 
-    int[] rowCount;
-    int[] colCount;
-    int boardSize;
-    int diagnol;
-    int antiDiagnol;
+
+class Solution {
+  
+    int[] rowCounter;
+    int[] columnCounter;
+    int boardSize, diagnoal = 0, antiDiagnol = 0;
+    
     /** Initialize your data structure here. */
-    public TicTacToe(int n) {
-        rowCount = new int[n];
-        colCount = new int[n];
+    public Solution(int n) {
+        rowCounter = new int[n];
+        columnCounter = new int[n];
         boardSize = n;
     }
     
@@ -21,22 +24,27 @@ class TicTacToe {
                 1: Player 1 wins.
                 2: Player 2 wins. */
     public int move(int row, int col, int player) {
-        int move = player == 1 ? 1 : -1;
-        rowCount[row] += move;
-        colCount[col] += move;
-        if(row == col) diagnol += move;
-        if(row+col == boardSize - 1) antiDiagnol+= move;
-        if(Math.abs(rowCount[row]) == boardSize || 
-           Math.abs(colCount[col]) == boardSize || 
-           Math.abs(diagnol) == boardSize || 
-           Math.abs(antiDiagnol) == boardSize) 
-            return player;
-        return 0;
+      int move = player == 1 ? 1 : -1;
+      rowCounter[row] += move;
+      columnCounter[row] += move;
+      if(row == col) diagnoal += move;
+      if(row + col == boardSize - 1) antiDiagnol += move;
+      if(Math.abs(rowCounter[row]) == boardSize ||
+         Math.abs(columnCounter[col]) == boardSize ||
+         Math.abs(diagnoal) == boardSize ||
+         Math.abs(antiDiagnol) == boardSize)
+        return player;
+      return 0;
     }
-}
 
-/**
- * Your TicTacToe object will be instantiated and called as such:
- * TicTacToe obj = new TicTacToe(n);
- * int param_1 = obj.move(row,col,player);
- */
+  public static void main(String[] args) {
+    Solution s = new Solution(3);
+    s.move(0, 0, 1);
+    s.move(0, 2, 2);
+    s.move(2, 2, 1);
+    s.move(1, 1, 2);
+    s.move(2, 0, 1);
+    s.move(1, 0, 2);
+    System.out.println(s.move(2, 1, 1));
+  }
+}
