deabfcae233c8b21d31745720fc6a020dbf3d601
Chiranjeevi Saride <chiranjeevisaride@gmail.com>
2021-02-10 17:26:22 -0500

    lps

--- DynamicProgramming/CountPalindromicSubStringsBottomUp.java
+++ DynamicProgramming/CountPalindromicSubStringsBottomUp.java
@@ -10,24 +10,41 @@ class Solution {
   private static int lps(String str) {
      boolean[][] dp = new boolean[str.length()][str.length()];
       
-     int maxLength = 1;
+      int maxLength = 1;
     
-     for(int startIndx = 0; startIndx < str.length(); startIndx++) 
+     for(int startIndx = 0; startIndx < str.length(); startIndx++) {
+         maxLength++;   
          dp[startIndx][startIndx] = true;
-     
+     } 
+        
+       
       
       for(int startIndx = str.length()-1; startIndx >= 0; startIndx--) {
         for(int endIndx = startIndx + 1; endIndx < str.length(); endIndx++) {
           if(str.charAt(startIndx) == str.charAt(endIndx)) {
             if(endIndx - startIndx == 1 || dp[startIndx+1][endIndx-1]) {
                 dp[startIndx][endIndx] = true;
-                maxLength = Math.max(maxLength, endIndx - startIndx + 1); 
+                maxLength++;  
             }  
           }
         } 
-      }      
+      } 
+      
+    /*
+      for(int startIndx = 0; startIndx < str.length(); startIndx++) {
+        for(int endIndx = 0; endIndx < str.length(); endIndx++) {
+          System.out.print(dp[startIndx][endIndx] + " ");
+        } 
+         System.out.println();
+      }  
+      */
+       
        return maxLength;
   }  
+  
+   
+  
+  
   
   public static void main(String[] args) {
      System.out.println(lps("abdbca"));
