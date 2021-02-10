540321217ae2468f55bcc31d116cbe2db00c2012
Chiranjeevi Saride <chiranjeevisaride@gmail.com>
2021-02-10 08:28:55 -0500

    lps

--- DynamicProgramming/LongestPalindromicSubStringBottomUp.java
+++ DynamicProgramming/LongestPalindromicSubStringBottomUp.java
@@ -1,25 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+
 class Solution {
   
-    private static int lps(String str) {
+  private static int lps(String str) {
+     boolean[][] dp = new boolean[str.length()][str.length()];
       
-      boolean[][] dp = new boolean[str.length()][str.length()];
-      
-      for(int startIndx = 0; startIndx < str.length(); startIndx++)
-        dp[startIndx][startIndx] = true;
-      int maxLength = 0;
+      int maxLength = 1;
       
       for(int startIndx = str.length()-1; startIndx >= 0; startIndx--) {
-        for(int endIndx = startIndx + 1; endIndx < str.length(); endIndx++) {
-          if(str.charAt(startIndx) == str.charAt(endIndx)) {
-             if(endIndx - startIndx == 1 || dp[startIndx+1][endIndx-1]) {
-                 dp[startIndx][endIndx] = true;
-                maxLength = Math.max(maxLength, endIndx - startIndx + 1); 
-             }   
+        for(int endIndx = startIndx ; endIndx < str.length(); endIndx++) {
+          if(startIndx == endIndx) 
+            dp[startIndx][startIndx] = true;
+          else if(str.charAt(startIndx) == str.charAt(endIndx) && 
+                   dp[startIndx+1][endIndx-1]) {
+             dp[startIndx][endIndx] = true;
+             maxLength = Math.max(maxLength, endIndx - startIndx + 1);    
           } 
         } 
       }  
       
-      /*
+    /*
       for(int startIndx = 0; startIndx < str.length(); startIndx++) {
         for(int endIndx = 0; endIndx < str.length(); endIndx++) {
           System.out.print(dp[startIndx][endIndx] + " ");
@@ -29,13 +31,12 @@ class Solution {
       */
        
        return maxLength;
-    }
-    
-    
-    
-    public static void main(String[] args) {
-       System.out.println(lps("cddpd"));
-       System.out.println(lps("aaaabbaa"));
-    }
-    
+  }  
+  
+  public static void main(String[] args) {
+     System.out.println(lps("abdbca"));
+     System.out.println(lps("cdpdd"));
+     System.out.println(lps("pqr"));
   }
+  
+}
