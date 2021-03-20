8944e97d406040a69bf54be5b8ff57ef88f99d2c
Chiranjeevi Saride <chiranjeevisaride@gmail.com>
2021-03-20 00:29:14 -0400

    20. Valid Parentheses

--- 20. Valid Parentheses.java
+++ 20. Valid Parentheses.java
@@ -31,3 +31,28 @@ class Solution {
         }
         return stack.isEmpty();
     }
+
+    // 3rd Approach 
+    class Solution {
+        public boolean isValid(String s) {
+            if(s == null || s.length() < 2) 
+                return false;
+            Stack<Character> stack = new Stack<>();
+            stack.push(s.charAt(0));
+            for(int index = 1; index < s.length(); index++) {
+                char c = s.charAt(index);
+                if(c == '(' || c == '{' || c == '[')
+                    stack.push(c);
+                else {
+                    if(stack.isEmpty() 
+                       || (c == ')' && stack.peek() != '(')
+                       || (c == '}' && stack.peek() != '{')
+                       || (c == ']' && stack.peek() != '['))
+                        return false;
+                    else
+                        stack.pop();
+                }
+            }
+            return stack.isEmpty();
+        }
+    }
