414efda72ea716e94bd9b395a961fbf20e839d61
Chiranjeevi Saride <chiranjeevisaride@gmail.com>
2021-04-13 18:38:55 -0400

    117. Populating Next Right Pointers in Each Node II

---
+++ 117. Populating Next Right Pointers in Each Node II.java
@@ -0,0 +1,43 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        if(root == null) return root;
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            for(int indx = 0; indx < size; indx++) {
+                Node current = queue.poll();
+                if(indx < size - 1) 
+                  current.next = queue.peek();
+                if(current.left != null)
+                    queue.add(current.left);
+                if(current.right != null)
+                    queue.add(current.right);
+            }
+        }
+        return root;
+    }
+}
